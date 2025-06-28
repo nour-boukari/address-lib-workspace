7632a36b00acb384b90e1b1c573fb5df198e121d
nour-boukari <nourelhouda.boukari@xevlabs.com>
2025-06-28 13:34:07 +0100

    formatting

--- projects/hls-address/src/public-api.ts
+++ projects/hls-address/src/public-api.ts
@@ -4,4 +4,4 @@
 
 export * from './lib/components/address-autocomplete/address-autocomplete';
 export * from './lib/types/models/address.model';
-export * from './lib/config/address-autocomplete-config'
+export * from './lib/config/address-autocomplete-config';
